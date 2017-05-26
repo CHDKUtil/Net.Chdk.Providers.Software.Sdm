8c9b46fc59f5fce93140917429f0f4f4fc02a120
Dmitry Shechtman <damnian@gmail.com>
2017-05-26 20:41:43 +0300

    Use ProductSourceProvider default implementations

--- src/Net.Chdk.Providers.Software.Sdm/SdmSourceProvider.cs
+++ src/Net.Chdk.Providers.Software.Sdm/SdmSourceProvider.cs
@@ -1,21 +1,9 @@
-﻿using System.Globalization;
-using Net.Chdk.Model.Software;
-using Net.Chdk.Providers.Software.Product;
+﻿using Net.Chdk.Providers.Software.Product;
 
 namespace Net.Chdk.Providers.Software.Sdm
 {
     sealed class SdmSourceProvider : ProductSourceProvider
     {
         protected override string ProductName => "SDM";
-
-        protected override string GetChannelName(SoftwareProductInfo product)
-        {
-            return null;
-        }
-
-        protected override CultureInfo GetLanguage(SoftwareSourceInfo source)
-        {
-            return null;
-        }
     }
 }
