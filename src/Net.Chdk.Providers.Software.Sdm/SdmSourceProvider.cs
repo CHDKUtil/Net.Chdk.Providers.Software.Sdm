444ab704f537cd7db734d558932e4bf1ed32e274
Dmitry Shechtman <damnian@gmail.com>
2017-05-27 20:04:27 +0300

    Add CategoryName

--- src/Net.Chdk.Providers.Software.Sdm/SdmSourceProvider.cs
+++ src/Net.Chdk.Providers.Software.Sdm/SdmSourceProvider.cs
@@ -4,6 +4,7 @@ namespace Net.Chdk.Providers.Software.Sdm
 {
     sealed class SdmSourceProvider : ProductSourceProvider
     {
+        protected override string CategoryName => "PS";
         protected override string ProductName => "SDM";
     }
 }
