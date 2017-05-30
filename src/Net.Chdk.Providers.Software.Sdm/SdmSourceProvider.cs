1022b07a57c18ce85d0660ad15d2a3fe740f6e98
Dmitry Shechtman <damnian@gmail.com>
2017-05-30 21:35:20 +0300

    Use DataProvider<T>

--- src/Net.Chdk.Providers.Software.Sdm/SdmSourceProvider.cs
+++ src/Net.Chdk.Providers.Software.Sdm/SdmSourceProvider.cs
@@ -1,9 +1,15 @@
-﻿using Net.Chdk.Providers.Software.Product;
+﻿using Microsoft.Extensions.Logging;
+using Net.Chdk.Providers.Software.Product;
 
 namespace Net.Chdk.Providers.Software.Sdm
 {
     sealed class SdmSourceProvider : ProductSourceProvider
     {
+        public SdmSourceProvider(ILoggerFactory loggerFactory)
+            : base(loggerFactory.CreateLogger<SdmSourceProvider>())
+        {
+        }
+
         protected override string CategoryName => "PS";
         protected override string ProductName => "SDM";
     }
