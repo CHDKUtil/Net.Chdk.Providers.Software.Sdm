47c18811e0ac019308f91237c07ac44e3958a635
Dmitry Shechtman <damnian@gmail.com>
2017-07-14 08:01:53 +0300

    Remove CategoryName

--- src/Net.Chdk.Providers.Software.Sdm/SdmSourceProvider.cs
+++ src/Net.Chdk.Providers.Software.Sdm/SdmSourceProvider.cs
@@ -1,17 +1,16 @@
 ﻿using Microsoft.Extensions.Logging;
+using Net.Chdk.Providers.Product;
 using Net.Chdk.Providers.Software.Product;
 
 namespace Net.Chdk.Providers.Software.Sdm
 {
     sealed class SdmSourceProvider : ProductSourceProvider
     {
-        public SdmSourceProvider(ILoggerFactory loggerFactory)
-            : base(loggerFactory.CreateLogger<SdmSourceProvider>())
+        public SdmSourceProvider(IProductProvider productProvider, ILoggerFactory loggerFactory)
+            : base(productProvider, loggerFactory.CreateLogger<SdmSourceProvider>())
         {
         }
 
-        public override string ProductName => "SDM";
-
-        protected override string CategoryName => "PS";
+        protected override string ProductName => "SDM";
     }
 }
